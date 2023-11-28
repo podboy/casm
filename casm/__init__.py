bf51e15885b62dc2cb4a6c4ee0489fa8d4598b3c
Mingzhe Zou <zoumingzhe@qq.com>
2023-11-28 19:53:11 +0800

    first version

---
+++ casm/__init__.py
@@ -0,0 +1,3 @@
+# coding:utf-8
+
+from .util import __version__
---
+++ casm/util.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+__package_name__ = "casm"
+__prog_name__ = "casm"
+__prog_compose__ = f"{__prog_name__}-compose"
+__version__ = "0.1.alpha.1"
+
+URL_PROG = "https://github.com/podboy/casm"
