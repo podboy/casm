733474561904d9124e23285c5e596d8982deea38
Mingzhe Zou <zoumingzhe@qq.com>
2023-12-02 01:47:18 +0800

    update setup.cfg

--- casm/utils/__init__.py
+++ casm/utils/__init__.py
@@ -18,6 +18,6 @@ from .podman_compose import podman_compose_cmd
 __package_name__ = "casm"
 __prog_name__ = "casm"
 __prog_compose__ = f"{__prog_name__}-compose"
-__version__ = "0.1.beta.1"
+__version__ = "0.1.beta.2"
 
 URL_PROG = "https://github.com/podboy/casm"
