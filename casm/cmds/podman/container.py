dc27c54c6343d30312d1acfd18bcedf81f0050f9
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-27 09:58:35 +0800

    improve container guard functionality and logging

--- casm/cmds/podman/container.py
+++ casm/cmds/podman/container.py
@@ -25,13 +25,19 @@ def add_cmd_container_guard(_arg: argp):
 
 @run_command(add_cmd_container_guard)
 def run_cmd_container_guard(cmds: commands) -> int:
+    def guard_container(container: podman_container) -> int:
+        cmds.logger.info("guard container %s begin", container.container_name)
+        exit_code: int = container.guard()
+        cmds.logger.info("guard container %s end, exit-code: %d",
+                         container.container_name, exit_code)
+        return exit_code
+
     container_names: Iterable[str] = cmds.args.containers or podman_container.list(all=True)  # noqa:E501
     containers: List[podman_container] = [podman_container(name) for name in container_names]  # noqa:E501
     for container in containers:
-        cmds.logger.info(f"guard container {container.container_name}")
-        exit_code: int = container.guard()
-        if exit_code != 0:
-            cmds.logger.error(f"failed to guard container {container.container_name} (exit-code: {exit_code})")  # noqa:E501
+        if (exit_code := guard_container(container)) != 0:
+            cmds.logger.error("failed to guard container %s",
+                              container.container_name)
             return exit_code
     return 0
 
