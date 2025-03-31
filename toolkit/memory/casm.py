0ce0eeceaa01e44bd12f1fecbd790896ac18c4ee
Mingzhe Zou <zoumingzhe@outlook.com>
2025-03-31 09:27:50 +0800

    toolkit: add memory monitoring scripts

---
+++ toolkit/memory/casm.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+
+import os
+
+from psutil import Process
+
+
+def memory_info(process: Process):
+    memory_info = process.memory_info()
+    print(f"VMS (Virtual Memory Size)\t{memory_info.vms / 1024 / 1024:.2f} MB\t{memory_info.vms}")  # noqa:E501
+    print(f"RSS (Resident Set Size)  \t{memory_info.rss / 1024 / 1024:.2f} MB\t{memory_info.rss}")  # noqa:E501
+
+
+def main():
+    pid: int = os.getpid()
+    print(f"Hello, PID {pid}")
+    process: Process = Process(pid)
+    print("Before import:")
+    memory_info(process)
+    from casm.cmds import main  # noqa:F401
+    print("After import:")
+    memory_info(process)
+    print("Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
