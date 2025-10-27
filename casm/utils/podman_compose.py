0ad9a64f04adb983a90d12125c700a30128c8205
Mingzhe Zou <zoumingzhe@outlook.com>
2025-10-27 14:18:35 +0800

    utils: add debug mode support to podman_compose_cmd

--- casm/utils/podman_compose.py
+++ casm/utils/podman_compose.py
@@ -8,21 +8,26 @@ from typing import Optional
 class podman_compose_cmd:
     """Execute podman_compose command"""
 
-    def __init__(self, compose_file: Optional[str]):
+    def __init__(self, compose_file: Optional[str], debug: bool = False):
         if isinstance(compose_file, str):
-            assert os.path.isfile(compose_file)
+            assert os.path.isfile(compose_file), f"'{compose_file}' is not a file"  # noqa:E501
         self.__compose_file: Optional[str] = compose_file
+        self.__print_debugging_output: bool = debug
 
     @property
     def opt_file(self) -> Optional[str]:
         return f"--file {self.__compose_file}" if self.__compose_file else None
+
+    @property
+    def opt_verbose(self) -> Optional[str]:
+        return "--verbose" if self.__print_debugging_output else None
 
     def run(self, *args: str) -> int:
         def exec(*cmds: Optional[str]) -> int:
             cmd = " ".join([c for c in cmds if c is not None])
             assert isinstance(cmd, str)
             return os.system(cmd)
-        cmds: List[Optional[str]] = ["podman-compose", self.opt_file]
+        cmds: List[Optional[str]] = ["podman-compose", self.opt_file, self.opt_verbose]  # noqa:E501
         cmds.extend([i for i in args if len(i) > 0])
         return exec(*cmds)
 
